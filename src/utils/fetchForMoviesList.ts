8ec95764079c344bdcc1bd84e2b915d409f46f0e
Iwo Matecki <iwo@iwomi.pl>
2022-07-04 10:51:10 +0200

    Improve fetchForMovieList function, add number of movies to fetch parameters

--- src/utils/fetchForMoviesList.ts
+++ src/utils/fetchForMoviesList.ts
@@ -1,10 +1,18 @@
 import { MoviesInDataBase } from 'types';
 import {config} from './config/config';
 
-export const fetchForMoviesList = async (): Promise<MoviesInDataBase[] | []> =>{
+export const fetchForMoviesList = async (number: number): Promise<MoviesInDataBase[] | []> =>{
     const path  = config.listOfMoviesUpdatePath;
     try {
-    const results: any = await fetch(path);
+    const results = await fetch(path, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            number,
+        }),
+    });
 
     return await results.json();
     } catch(err){
