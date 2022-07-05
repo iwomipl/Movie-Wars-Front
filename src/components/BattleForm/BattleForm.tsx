ae7823a8d214b8b892c6b75ed65c6b59661b6573
Iwo Matecki <iwo@iwomi.pl>
2022-07-05 09:16:10 +0200

    Create BattleForm component

---
+++ src/components/BattleForm/BattleForm.tsx
@@ -0,0 +1,63 @@
+import React, {ChangeEvent, FormEvent} from "react";
+import {CommonInput} from "../common/CommonInput";
+
+interface Props {
+    submitFunction:(e: FormEvent<HTMLFormElement>)=> Promise<void>;
+    changeFunction:(e: ChangeEvent<HTMLInputElement>)=> Promise<void>;
+}
+
+export const BattleForm = (props:  Props)=>{
+
+
+    return         <form onSubmit={props.submitFunction} id="battles">
+        <CommonInput
+            text="15 Battles"
+            type="radio"
+            value={15}
+            name="battles"
+            function={(e: ChangeEvent<HTMLInputElement>) => props.changeFunction(e)}
+            className="battleInput"
+        /><br/>
+        <CommonInput
+            text="31 Battles"
+            type="radio"
+            value={31}
+            name="battles"
+            function={(e: ChangeEvent<HTMLInputElement>) => props.changeFunction(e)}
+            className="battleInput"
+        /><br/>
+        <CommonInput
+            text="63 Battles"
+            type="radio"
+            value={63}
+            name="battles"
+            function={(e: ChangeEvent<HTMLInputElement>) => props.changeFunction(e)}
+            className="battleInput"
+        /><br/>
+        <CommonInput
+            text="127 Battles"
+            type="radio"
+            value={127}
+            name="battles"
+            function={(e: ChangeEvent<HTMLInputElement>) => props.changeFunction(e)}
+            className="battleInput"
+        /><br/>
+        <CommonInput
+            text="255 Battles"
+            type="radio"
+            value={255}
+            name="battles"
+            function={(e: ChangeEvent<HTMLInputElement>) => props.changeFunction(e)}
+            className="battleInput"
+        /><br/>
+        <CommonInput
+            text="511 Battles"
+            type="radio"
+            value={511}
+            name="battles"
+            function={(e: ChangeEvent<HTMLInputElement>) => props.changeFunction(e)}
+            className="battleInput"
+        /><br/>
+        <button>Start!</button>
+    </form>
+}
--- src/components/BattleView/BattleView.tsx
+++ src/components/BattleView/BattleView.tsx
@@ -3,81 +3,34 @@ import {setMoviesList} from "../../features/moviesList/moviesList.slice";
 import {fetchForMoviesList} from "../../utils/fetchForMoviesList";
 import {useDispatch} from "react-redux";
 import {MoviesList} from "../MoviesList/MoviesList";
-import {CommonInput} from "../common/CommonInput";
+import { BattleForm } from '../BattleForm/BattleForm';
 
-export const BattleView = ()=>{
+export const BattleView = () => {
     const dispatch = useDispatch();
     const [showForm, setShowForm] = useState(true);
 
-    const [some, setSome] =  useState(0);
+    const [some, setSome] = useState(0);
 
     const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        dispatch(setMoviesList(await fetchForMoviesList(Math.ceil(some/2), 'POST')));
+        dispatch(setMoviesList(await fetchForMoviesList(Math.ceil(some / 2), 'POST')));
         setShowForm(false)
         console.log(e.target);
 
 
     }
 
-    const chooseMovies = async (e: ChangeEvent<HTMLInputElement>)=> {
+    const chooseMovies = async (e: ChangeEvent<HTMLInputElement>) => {
         setSome(Number(e.target.value));
     }
 
     return <>
         <h3>How many of best movies do You want to compare?</h3>
         {showForm ?
-        <form onSubmit={handleSubmit} id="battles">
-            <CommonInput
-                text="15 Battles"
-                type="radio"
-                value={15}
-                name="battles"
-                function={(e: ChangeEvent<HTMLInputElement>) => chooseMovies(e)}
-                className="battleInput"
-            /><br/>
-            <CommonInput
-                text="31 Battles"
-                type="radio"
-                value={31}
-                name="battles"
-                function={(e: ChangeEvent<HTMLInputElement>) => chooseMovies(e)}
-                className="battleInput"
-            /><br/>
-            <CommonInput
-                text="63 Battles"
-                type="radio"
-                value={63}
-                name="battles"
-                function={(e: ChangeEvent<HTMLInputElement>) => chooseMovies(e)}
-                className="battleInput"
-            /><br/>
-            <CommonInput
-                text="127 Battles"
-                type="radio"
-                value={127}
-                name="battles"
-                function={(e: ChangeEvent<HTMLInputElement>) => chooseMovies(e)}
-                className="battleInput"
-            /><br/>
-            <CommonInput
-                text="255 Battles"
-                type="radio"
-                value={255}
-                name="battles"
-                function={(e: ChangeEvent<HTMLInputElement>) => chooseMovies(e)}
-                className="battleInput"
-            /><br/>
-            <CommonInput
-                text="511 Battles"
-                type="radio"
-                value={511}
-                name="battles"
-                function={(e: ChangeEvent<HTMLInputElement>) => chooseMovies(e)}
-                className="battleInput"
-            /><br/>
-            <button>Best 16</button>
-        </form> :
+            <BattleForm
+            submitFunction={(e: FormEvent<HTMLFormElement>) => handleSubmit(e)}
+            changeFunction={(e: ChangeEvent<HTMLInputElement>) => chooseMovies(e)}
+            /> :
             <MoviesList/>
         }
         {some ? some : null}
