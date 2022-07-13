519ce21944e30adcb00c20b95c22b5eede1650cb
Iwo Matecki <iwo@iwomi.pl>
2022-07-13 20:14:44 +0200

    Minor changes in contents

--- src/components/MainView/MainView.tsx
+++ src/components/MainView/MainView.tsx
@@ -5,13 +5,13 @@ export const MainView = () => {
 
     return <>
         <h1>Hi there!</h1>
-        <p>This website is for people who like Movies, and are willing to play a little game to find out, which movie do they like the most.</p>
-        <h3>The mechanizm in Movie Wars is simple.</h3>
-        <p>First choose how many movies do you want to compare.</p>
-        <p>Than wi will show you pairs of movie. Your job is to choose between them. Which one is better.</p>
-        <p>You will be having almost two times more battles, than movies, so choose carefully.</p>
-        <h4>Our goal is to choose the best one.</h4>
-        <em> Remember, that you don't know all movies, and the more movies you will the greater chance to not know the movie.</em>
-        <h3>Godspeed, and have fun with movies!</h3>
+        <p>This website is for people who like Movies and are willing to play a little game to find out, which movie they like the most.</p>
+        <h3>The mechanism in Movie Wars is very simple.</h3>
+        <p>First choose how many movies you want to compare.</p>
+        <p>Than I will show you pairs of movies. Your job is to choose between them. Which one is better for You.</p>
+        <p>You will be having almost two times more battles, than movies, so choose number of movies carefully.</p>
+        <h4>Our goal is to find out which one is the best one for You.</h4>
+        <em>Remember, that you don't know all movies and the more movies you will try to compare, the greater the chance to not know the movie.</em>
+        <h3>Godspeed and have fun with movies!</h3>
     </>
 }
--- src/components/ShowWinnerMovie/ShowWinnerMovie.tsx
+++ src/components/ShowWinnerMovie/ShowWinnerMovie.tsx
@@ -7,8 +7,8 @@ interface Props extends Omit<MoviesInDataBase, 'rated' | 'polTitle' | 'position'
 
 export const ShowWinnerMovie = (props: Props) => {
     return <div className="lastMovie">
-        <h3>That's it, you have chosen best Movie.</h3>
-        <h2>The winner is:</h2>
+        <h3>That's it, you have chosen best Movie for You.</h3>
+        <h2>Your winner is:</h2>
         <div className="movieInBattle good-shadow">
             <img src={props.poster} alt={props.origTitle} className="movieImg"/>
             <div className="movie-desc">
