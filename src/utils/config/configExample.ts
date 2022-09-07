4c381d59b81af0b24a80c4ad416e7df16b54bac7
Iwo Matecki <iwo@iwomi.pl>
2022-09-07 12:50:17 +0200

    Change config file to set path to Api in one place, also add urlPrefix if one exists

--- src/utils/config/configExample.ts
+++ src/utils/config/configExample.ts
@@ -1,3 +1,5 @@
+const yourBackendDomain = 'http://localhost:3001'; // 'http://localhost:3001' for localhost on port 3001
+const yourBackendDomainUrlPrefix = ''; // empty string '' if mo prefix "/new-api" if your domain is localhost 'http://yourDomain.com/new-api'
 export const config = {
-    listOfMoviesUpdatePath: '<yourhost>/get-list',
+    listOfMoviesUpdatePath: `${yourBackendDomain}${yourBackendDomainUrlPrefix}`,
 }
